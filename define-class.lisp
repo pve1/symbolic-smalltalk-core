706eca237e5f02d5e23aabc82962f12a11806386
pve1 <pve1@foo.invalid>
2023-05-29 10:52:10 +0300

    Clean up define-class.

--- define-class.lisp
+++ define-class.lisp
@@ -4,8 +4,7 @@
 
 (defmacro define-class (name superclass instance-variables class-variables)
   `(eval-when (:compile-toplevel :load-toplevel :execute)
-     (m::subclass/instance-variable-names/class-variable-names/3
-      (the-class ',superclass)
-      ',name
-      ',instance-variables
-      ',class-variables)))
+     (send (the-class ',superclass)
+       :subclass ',name
+       :instance-variable-names ',instance-variables
+       :class-variable-names ',class-variables)))
