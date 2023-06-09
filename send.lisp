72154223da1fa0c2ac64e459eafef058031500a2
pve1 <pve1@foo.invalid>
2023-06-09 20:43:19 +0300

    Add call-super.

--- send.lisp
+++ send.lisp
@@ -1,9 +1,23 @@
 (in-package :symbolic-smalltalk-core)
 
+(defun call-super (method args)
+  (let* ((super-class
+           (class-name
+            (first (closer-mop:class-direct-superclasses (class-of (first args))))))
+         (super-method
+           (find super-class (closer-mop:compute-applicable-methods-using-classes
+                              method
+                              (mapcar #'class-of args))
+                 :key (lambda (x)
+                        (class-name (first (closer-mop:method-specializers x)))))))
+    (funcall (closer-mop:method-function super-method) args nil)))
+
 (defmacro send (recipient &body message)
   (if (and (symbolp recipient)
            (string= "SUPER" recipient))
-      `(call-next-method ,(self) ,@(extract-parameters-from-message message))
+      (let ((gf (translate-message message)))
+        `(call-super #',gf (list ,(self) ,@(extract-parameters-from-message
+                                            message))))
       (translate-send recipient message)))
 
 (defmacro send-class (recipient &body message)
